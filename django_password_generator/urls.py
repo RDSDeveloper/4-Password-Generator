bdec09014739f812588d01b266eae50c36b5f5c2
Rodrigo Damian Santos <106984817+RDSDeveloper@users.noreply.github.com>
2022-10-17 14:50:16 -0300

    Final Stage

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("about", views.about),
+    path("about", views.about, name="about"),
+    path("", views.home, name="home"),
+    path("generate-password", views.password, name="password"),
 ]
